6ab8ea6303ed6a477ff89fe05780b7902025967b
Pierre Convert <pierreconvert@macbook-pro-6.home>
2024-10-29 17:14:04 +0100

    Updates

--- sources/com/s8/pkgs/io/svg/styles/SVG_Fill.java
+++ sources/com/s8/pkgs/io/svg/styles/SVG_Fill.java
@@ -1,10 +1,12 @@
 package com.s8.pkgs.io.svg.styles;
 
-public class Fill {
+
+
+public class SVG_Fill {
 	
 	public final SVG_FillColor color;
 
-	public Fill(SVG_FillColor color) {
+	public SVG_Fill(SVG_FillColor color) {
 		super();
 		this.color = color;
 	}
